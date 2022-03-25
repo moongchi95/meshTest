37af349b2212a62a1ffe78f472bb2fdfad567b10
minhyung <mpsmhck95@naver.com>
2022-03-25 12:42:40 +0900

    test

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('test');
+let click_plus;
+
+$(function(){
+    $('.spinner .spinner_minus').on('click',function(){
+        $(this).siblings('.spinner_input').val(Number($(this).siblings('.spinner_input').val())-1);
+    })
+    $('.spinner .spinner_plus').on('mousedown',function(){
+        click_plus = setInterval(() => {
+            $(this).siblings('.spinner_input').val(Number($(this).siblings('.spinner_input').val())+1);
+        }, 50);
+    })
+    $('.spinner .spinner_plus').on('mouseup',function(){
+        clearInterval(click_plus);
+    })
+
+})
